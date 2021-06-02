503d37042580afe1321c17992ba2b2920159ce82
Irina Illustrova <illustrova@gmail.com>
2021-06-03 01:56:19 +0300

    Add coded components

--- design-system/src/index.js
+++ design-system/src/index.js
@@ -7,9 +7,12 @@
 export { default as GlobalStyle } from "./global.js";
 export { colors, typography, spacing } from "./tokens";
 
-
+export { default as Avatar } from "./components/Avatar";
 export { default as Button } from "./components/Button";
 export { default as Carousel } from "./components/Carousel";
 export { default as Icon } from "./components/Icon";
-export { default as Select } from "./components/Select";
-export { default as SummaryTable } from "./components/SummaryTable";
+export { default as DeliveryOptions } from "./components/DeliveryOptions";
+export { default as OrderSummary } from "./components/OrderSummary";
+export { default as NotificationLink } from "./components/NotificationLink";
+export { default as ProductCard } from "./components/ProductCard";
+export { default as TopBar } from "./components/TopBar";
