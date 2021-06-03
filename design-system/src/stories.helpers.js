ea9e5426267a82cfd98936d7683c30d5ed6906ca
tomi-sjoblom <deehawkie@gmail.com>
2021-06-03 12:18:25 +0300

    Start adding shadow docs

--- design-system/src/stories.helpers.js
+++ design-system/src/stories.helpers.js
@@ -92,11 +92,8 @@ export function RadiusSet({radiuses}) {
       return res;
     }, []);
   }
-  console.log(radiuses)
   const spacesSizes = getSizes(Object.entries(radiuses));
-  console.log(spacesSizes)
   const spacesGrouped = getGrouped(Object.entries(radiuses), spacesSizes);
-  console.log(spacesGrouped)
 
   return (
     <div>{spacesSizes.map(size => <RadiusBox key={size} size={size} spaces={spacesGrouped[size]} />)}</div>
@@ -130,5 +127,71 @@ export function RadiusBox({spaces}) {
       </RadiusUnitHolder>
       <code><strong>Size:</strong> {spaces[0][1]}, <strong>Variables:</strong> {spaces.map(item => item[0]).join(', ')}</code>
     </RadiusBoxStyled>
+  )
+}
+
+
+
+export function ShadowSet({shadows}) {
+
+  const groupBy = keys => array =>
+    array.reduce((objectsByKeyValue, obj) => {
+      const value = keys.map(key => obj[key]).join('-');
+      objectsByKeyValue[value] = (objectsByKeyValue[value] || []).concat(obj);
+      return objectsByKeyValue;
+    }, {});
+
+  const getGrouped = (arr, sizes) => {
+
+    return sizes.reduce((acc,curr)=> (acc[curr]=arr.filter(s => s[0].indexOf(`shadow${curr}`) !== -1),acc),{});
+  }
+
+  const getShadow = (arr) => {
+    return arr.reduce((res, obj) => {
+      const shadow = obj[0].split('shadow')[1].split(/(Type|Radius|Color|OffsetX|OffsetY|Spread)/)[0];
+      console.log(shadow)
+      if (res.indexOf(shadow) === -1 ) res.push(shadow);
+      return res;
+    }, []);
+  }
+  console.log(shadows)
+  const spacesSizes = getShadow(Object.entries(shadows));
+  console.log(spacesSizes)
+  const spacesGrouped = getGrouped(Object.entries(shadows), spacesSizes);
+  console.log(spacesGrouped)
+
+  return (
+    <div>{spacesSizes.map(size => <ShadowBox key={size} size={size} shadows={spacesGrouped[size]} />)}</div>
+  )
+}
+
+const ShadowBoxStyled = styled.div`
+  margin-bottom: 2em;
+  display: flex;
+`;
+
+const ShadowUnitHolder = styled.div`
+  width: 116px;
+  display: inline-block;
+  flex-shrink: 0;
+`;
+
+const ShadowUnit = styled.div`
+  width: 100px;
+  height: 100px;
+  border-radius: 16px;
+  display: inline-block;
+  background-color: ${(colors.primary500 || 'red')};
+  box-shadow: ${({ boxshadow }) => boxshadow};
+`;
+
+export function ShadowBox({shadows}) {
+  return (
+    <ShadowBoxStyled>
+      <ShadowUnitHolder>
+        <ShadowUnit boxshadow={shadows[0][1]}/>
+      </ShadowUnitHolder>
+      <code>Variables: {shadows.map(item => item[0]).join(', ')}</code>
+    </ShadowBoxStyled>
   )
 }
